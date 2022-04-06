295763acb070c68cfa298aa2c25543a5d1bb1b5d
charles okunzo <okunzopeter01@gmail.com>
2022-04-06 14:41:44 +0300

    feat:deleting goal-add functionality to delete goal

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -11,7 +11,11 @@ export class GoalDetailComponent implements OnInit {
   @Input() goal!:Goal;
   @Output() isComplete= new EventEmitter<boolean>();
 
-  goalComplete(complete:boolean){
+  // goalComplete(complete:boolean){
+  //   this.isComplete.emit(complete);
+  // }
+
+  goalDelete(complete:boolean){
     this.isComplete.emit(complete);
   }
 
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -30,8 +30,17 @@ export class GoalComponent implements OnInit {
     this.goals[index].showDescription= !this.goals[index].showDescription;
   }
 
-  completeGoal(isComplete:boolean, index:any){
-    if(isComplete){
+  // completeGoal(isComplete:boolean, index:any){
+  //   if(isComplete){
+  //     this.goals.splice(index, 1)
+  //   }
+  // }
+
+
+  deleteGoal(isComplete:boolean,index:any){
+    let toDelete= confirm(`Are you sure you want to delete ${this.goals[index].name}`)
+
+    if(toDelete){
       this.goals.splice(index, 1)
     }
   }
